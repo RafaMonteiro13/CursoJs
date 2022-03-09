0a40f52af102de25f5f535ee4b94c49a2ad06e94
unknown <rafaelmonteirocaldas@gmail.com>
2022-03-09 09:33:52 -0300

    Aula 11, ex 8, 9, 10 e 11

---
+++ aula11/Ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/Hr`)
+if (vel > 55) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Velocidade dentro do permitido')
+}
+
+console.log('Dirija sempre usando conto de segurança')
